a3ce497335814f12c7d342c910268ab0d762871f
Gary Guo <gary.guo@ibm.com>
2021-02-21 13:14:33 -0500

    add a c pthread example

---
+++ c/multi-thread.c
@@ -0,0 +1,59 @@
+#include<stdio.h>    
+#include<stdlib.h>    
+#include<string.h>    
+#include<pthread.h>
+#include <unistd.h>
+
+#define NUM_THREADS 5
+
+struct thread_data
+{
+  int  thread_id;
+  char data[50];
+};
+
+
+void *hello(void *threadarg)
+{
+  struct thread_data *myData;
+
+  myData = (struct thread_data *) threadarg;
+  printf("thread[%d], hello: %s\n", myData->thread_id, myData->data);
+  usleep(100);
+  return NULL;
+}
+
+int main ()
+{
+  pthread_t threads[NUM_THREADS];
+
+  struct thread_data td[NUM_THREADS];
+
+  int rc, i;
+
+  for(i = 0; i < NUM_THREADS; i++)
+  {
+    td[i].thread_id = i;
+
+    sprintf(td[i].data, "This is message - [%d]", i);
+
+    rc = pthread_create(&threads[i], NULL,
+
+        hello, (void *)&td[i]);
+    // pthread_join(threads[i], NULL);
+
+    if (rc){
+
+      printf("Error:unable to create thread, %d\n", rc);
+
+      exit(-1);
+    }
+  }
+  for(i = 0; i < NUM_THREADS; i++)
+  {
+    pthread_join(threads[i], NULL);
+  }
+
+  pthread_exit(NULL);
+  return 0;
+}
